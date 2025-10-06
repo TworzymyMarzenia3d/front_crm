bc3b5c309558201c2343e292284a749028500f97
Marcin Szafraniec <marcin.szafraniec@icloud.com>
2025-10-06 22:13:57 +0200

    fix: column Clients

--- src/Clients.js
+++ src/Clients.js
@@ -93,7 +93,7 @@ function Clients() {
 
     return (
         <>
-            <div className="management-panel" style={{alignItems: 'flex-start', maxWidth: '500px'}}>
+            <div className="management-panel" >
                 <section className="form-section">
                     <h2>{isEditing ? 'Edytuj klienta' : 'Dodaj nowego klienta'}</h2>
                     <form onSubmit={handleSubmit}>
